fda1791aebab9ac110fc45e413b5b135a1bbf908
Rafael Moraes Trevizan <rafaeltrevizan@alunos.utfpr.edu.br>
2025-03-24 14:41:13 -0300

    ínicio do script.js e adicao da funcionalidade pro botao inicio

---
+++ script.js
@@ -0,0 +1,8 @@
+let botaoInicio = document.getElementById("botaoinicio");
+let botaoServicos = document.getElementById("botaosevico");
+let botaoSobre = document.getElementById("botaosobre");
+let botaoContato = document.getElementById("botaocontato");
+
+botaoInicio.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});

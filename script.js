71b416033bbec4950115b55afc6b071d995a2339
João Vitor <romanijoaovitor@gmail.com>
2025-03-24 15:37:12 -0300

    insere radio buttons na div do carrossel

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Início botões de navegação da página
 let botaoInicio = document.getElementById("botaoinicio");
 let botaoServicos = document.getElementById("botaosevico");
 let botaoSobre = document.getElementById("botaosobre");
@@ -6,3 +7,4 @@ let botaoContato = document.getElementById("botaocontato");
 botaoInicio.addEventListener("click", () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
 });
+// Fim botões de navegação da página

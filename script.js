5e2d79b3a1c1b13e14e90734d597c9b02f9ecdaa
Rafael Moraes Trevizan <rafaeltrevizan@alunos.utfpr.edu.br>
2025-03-24 14:14:24 -0300

    adicao do script.js e inicio das funcionalidades dos botoes

---
+++ script.js
@@ -0,0 +1,26 @@
+let botaoInicio = document.getElementById("botaoinicio");
+let botaoServicos = document.getElementById("botaoservicos");
+let botaoSobre = document.getElementById("botaosobre");
+let botaoContato = document.getElementById("botaocontato");
+
+
+
+botaoInicio.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+
+botaoServicos.addEventListener("click", () => {
+    ;
+});
+
+
+botaoSobre.addEventListener("click", () => {
+    ;
+});
+
+
+botaoContato.addEventListener("click", () => {
+    ;
+});
+

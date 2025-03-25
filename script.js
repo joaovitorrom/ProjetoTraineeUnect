fa71a8fc4ef98c82fb142bb3632c949f01614a96
João Vitor <romanijoaovitor@gmail.com>
2025-03-24 23:27:23 -0300

    adiciona funções de esconder/mostrar os banners do carrossel

--- script.js
+++ script.js
@@ -8,3 +8,27 @@ botaoInicio.addEventListener("click", () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
 });
 // Fim botões de navegação da página
+
+// Início botões de transição do carrossel
+const flechaAnterior = document.getElementById("arrowBack");
+const flechaSeguinte = document.getElementById("arrowForward");
+const botao1 = document.getElementById("radio-bt1");
+const botao2 = document.getElementById("radio-bt2");
+const botao3 = document.getElementById("radio-bt3");
+const banners = document.querySelectorAll(".modelBanners");
+
+let indexAtual = 0;
+
+function hideBanner () { // Função para esconder os banners
+    banners.forEach((banner) => {
+        banner.classList.remove("on");
+});
+
+function showBanner () { // Função para mostrar os banners
+    banners.forEach((banner) => {
+        banner[indexAtual].classList.add("on");
+});
+
+
+
+// Fim botões de transição do carrossel

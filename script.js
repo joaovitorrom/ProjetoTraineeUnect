6a30c66e1bddc2c933b62485628e72fdee01b36a
João Vitor <romanijoaovitor@gmail.com>
2025-03-25 16:33:35 -0300

    adiciona funções que sincronizam os seletores com os slides

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ const botao1 = document.getElementById("radio-btn1");
 const botao2 = document.getElementById("radio-btn2");
 const botao3 = document.getElementById("radio-btn3");
 const banners = document.querySelectorAll(".modelBanners");
+const botoes = document.querySelectorAll('input[type="radio"]');
 
 let indexAtual = 0;
 
@@ -32,29 +33,51 @@ function showBanner () { // Função para mostrar os banners
     banners[indexAtual].style.display = 'block';
 };
 
+function hideBannerButton () { // Função para esconder os banners ao selecionar um radio button
+    botoes.forEach(button => {
+        button.checked = false;
+    });
+};
+
+function showBannerButton () { // Função para mostrar os banners ao selecionar um radio button
+    botoes[indexAtual].checked = true;
+    botoes[indexAtual].style.display = 'block';
+};
+
 function nextBanner () { // Função para avançar o banner
     hideBanner();
+    hideBannerButton();
+
     if (indexAtual === banners.length - 1) {
         indexAtual = 0;
     } else {
         indexAtual++;
     }
+
     showBanner();
+    showBannerButton();
 };
 
 function previousBanner () { // Função para retroceder o banner
     hideBanner();
+    hideBannerButton();
+
     if (indexAtual === 0) {
         indexAtual = banners.length - 1;
     } else {
         indexAtual--;
     }
+
     showBanner();
+    showBannerButton();
 };
+
 
 
 flechaAnterior.addEventListener("click", previousBanner); // Evento para retroceder o banner
 
 flechaSeguinte.addEventListener("click", nextBanner); // Evento para avançar o banner
+
+
 
 // Fim botões de transição do carrossel

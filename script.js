078c6d9b936ebf13ee59e60bcc17bbb46d82e1ed
João Vitor <romanijoaovitor@gmail.com>
2025-03-25 14:38:46 -0300

    transforma flechas em botões e adiciona funções de verificação +  eventos do carrossel para elas

--- script.js
+++ script.js
@@ -12,23 +12,49 @@ botaoInicio.addEventListener("click", () => {
 // Início botões de transição do carrossel
 const flechaAnterior = document.getElementById("arrowBack");
 const flechaSeguinte = document.getElementById("arrowForward");
-const botao1 = document.getElementById("radio-bt1");
-const botao2 = document.getElementById("radio-bt2");
-const botao3 = document.getElementById("radio-bt3");
+const botao1 = document.getElementById("radio-btn1");
+const botao2 = document.getElementById("radio-btn2");
+const botao3 = document.getElementById("radio-btn3");
 const banners = document.querySelectorAll(".modelBanners");
 
 let indexAtual = 0;
 
 function hideBanner () { // Função para esconder os banners
-    banners.forEach((banner) => {
-        banner.classList.remove("on");
-});
+    banners.forEach(banner => {
+        banner.classList.remove('on');
+        if (!banner.classList.contains('on')) {
+            banner.style.display = 'none';
+        }
+    });
+};
 
 function showBanner () { // Função para mostrar os banners
-    banners.forEach((banner) => {
-        banner[indexAtual].classList.add("on");
-});
+    banners[indexAtual].style.display = 'block';
+    banners[indexAtual].classList.add('on');
+};
 
+function nextBanner () { // Função para avançar o banner
+    hideBanner();
+    if (indexAtual === banners.length - 1) {
+        indexAtual = 0;
+    } else {
+        indexAtual++;
+    }
+    showBanner();
+};
 
+function previousBanner () { // Função para retroceder o banner
+    hideBanner();
+    if (indexAtual === 0) {
+        indexAtual = banners.length - 1;
+    } else {
+        indexAtual--;
+    }
+    showBanner();
+};
+
+flechaAnterior.addEventListener("click", previousBanner); // Evento para retroceder o banner
+
+flechaSeguinte.addEventListener("click", nextBanner); // Evento para avançar o banner
 
 // Fim botões de transição do carrossel

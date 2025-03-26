b1569f58260c1b670b62edd1c55919a351277634
João Vitor <romanijoaovitor@gmail.com>
2025-03-26 15:04:48 -0300

    melhora a implementação das funções hideBanner e showBanner

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function hideBanner () { // Função para esconder os banners
     banners.forEach(banner => {
         banner.classList.remove('on');
     });
-    botoes.forEach(button => { // Desmarca todos radio buttons
+    botoes.forEach(button => { // desmarca todos radio buttons
         button.checked = false;
     });
 };
@@ -35,42 +35,25 @@ function showBanner () { // Função para mostrar os banners
     botoes[indexAtual].checked = true; // marca o radio button correspondente ao banner
 };
 
-/* function hideBannerButton () { // Função para desmarcar todos radio buttons
-    botoes.forEach(button => {
-        button.checked = false;
-    });
-}; */
-
-/*function showBannerButton () { // Função para marcar o radio button correspondente ao banner
-    botoes[indexAtual].checked = true;
-}; */
 
 function nextBanner () { // Função para avançar o banner
     hideBanner();
-   //hideBannerButton();
-
     if (indexAtual === banners.length - 1) {
         indexAtual = 0;
     } else {
         indexAtual++;
     }
-
     showBanner();
-    //showBannerButton();
 };
 
 function previousBanner () { // Função para retroceder o banner
     hideBanner();
-    //hideBannerButton();
-
     if (indexAtual === 0) {
         indexAtual = banners.length - 1;
     } else {
         indexAtual--;
     }
-
     showBanner();
-    //showBannerButton();
 };
 
 flechaAnterior.addEventListener("click", previousBanner); // Evento para retroceder o banner
@@ -78,26 +61,20 @@ flechaAnterior.addEventListener("click", previousBanner); // Evento para retroce
 flechaSeguinte.addEventListener("click", nextBanner); // Evento para avançar o banner
 
 botao1.addEventListener("click", () => {
-    hideBanner();       // Esconde todos os banners
-    //hideBannerButton(); // Desmarca todos os radio buttons
-    indexAtual = 0;     // Atualiza o índice para o primeiro slide
-    showBanner();       // Exibe o banner correspondente
-    //showBannerButton(); // Marca o radio button correspondente
+    hideBanner();
+    indexAtual = 0; // Atualiza o índice para o primeiro slide
+    showBanner();      
 });
 
 botao2.addEventListener("click", () => {
     hideBanner();
-    //hideBannerButton();
     indexAtual = 1;
     showBanner();
-    //showBannerButton();
 });
 
 botao3.addEventListener("click", () => {
     hideBanner();
-    //hideBannerButton();
     indexAtual = 2;
     showBanner();
-    //showBannerButton();
 });
 // Fim botões de transição do carrossel
